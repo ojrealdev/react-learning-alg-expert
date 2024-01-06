b8332d642f2326a2c305286765c3032b72866b3e
soojwang <soojwang@safaricom.co.ke>
2024-01-06 10:27:59 +0300

    react learning first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [isClicked, setIsClicked] = useState(false);
+
+	const setClick = (msg) => {
+		alert(msg);
+		setIsClicked(true);
+	};
+
+	return (
+		<div className='App-header'>
+			<header className='App-header'>
+				<h1>A Modal will appear if you click the button below</h1>
+				<button onClick={() => setClick('hello world!')}>Create Modal</button>
+				{isClicked ? (
+					<Modal
+						className='modal'
+						isClicked={isClicked}
+						setIsClicked={setIsClicked}
+					>
+						<button>click!</button>
+					</Modal>
+				) : null}
+			</header>
+		</div>
+	);
+};
+
+const Modal = ({ isClicked, setIsClicked, children }) => (
+	<div>
+		<p>This is a Modal!</p>
+		<button onClick={() => setIsClicked(false)}>Remove Modal!</button>
+		{children}
+	</div>
+);
 
 export default App;
